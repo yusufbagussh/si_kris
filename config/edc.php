c03297e6be91386759d41bbe10f9dc0055b2708d
yusufbagussh <yusuf.herlambang27@gmail.com>
2025-05-30 13:59:37 +0700

    fix:edc config

--- config/edc.php
+++ config/edc.php
@@ -11,16 +11,15 @@ return [
     */
 
     // EDC Address (IP of the EDC device)
-    'edc_address' => env('EDC_ADDRESS', 'localhost'), //10.100.3.11
+    'edc_address' => env('EDC_WEBSOCKET_ADDRESS', 'localhost'), //10.100.3.11
     // EDC Address (Port of the EDC device)
-    'edc_port' => env('EDC_PORT', '6745'),
+    'edc_port' => env('EDC_WEBSOCKET_PORT', '6745'),
     // POS Address (IP of the POS/application/APM App)
-    'pos_address' => env('EDC_POS_ADDRESS', 'localhost'), //192.167.4.250
+    'pos_address' => env('EDC_POS_WEBSOCKET_ADDRESS', 'localhost'), //192.167.4.250
     // Use secure WebSocket (WSS)
-    'secure' => env('EDC_SECURE', false),
+    'secure' => env('EDC_WEBSOCKET_SECURE', false),
     // Secret key for encryption (development)
     'secret_key' => env('EDC_SECRET_KEY', 'ECR2022secretKey'),
-
     'webhook_url' => env('EDC_WEBHOOK_BASE_URL'),
     'webhook_secret' => env('EDC_WEBHOOK_SECRET'),
 ];
